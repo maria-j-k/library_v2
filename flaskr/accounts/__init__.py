3bb06949ea194d582de3f969c2fb0f5a9ce5d945
maria <maria77julia@gmail.com>
2020-12-26 17:57:55 +0100

    flask-security default

--- flaskr/accounts/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('accounts', __name__)
-
-from flaskr.accounts import routes
--- flaskr/security/script.py
+++ flaskr/security/script.py
@@ -14,8 +14,6 @@ from flask_security.models import fsqla_v2 as fsqla
 from flaskr import db
 from flaskr.security import bp
 
-#from flaskr.security.forms import ReaderRegisterForm
-#from new_forms import RegisterForm
 
 
 class Role(db.Model, fsqla.FsRoleMixin):
@@ -23,18 +21,19 @@ class Role(db.Model, fsqla.FsRoleMixin):
 
 
 class User(db.Model, fsqla.FsUserMixin):
-    first_name = db.Column(db.String(32), nullable=False)
-    last_name = db.Column(db.String(32), nullable=False)
-    division_id = db.Column(db.Integer, db.ForeignKey('division.id'), nullable=True)
+    pass
+#    first_name = db.Column(db.String(32), nullable=False)
+#    last_name = db.Column(db.String(32), nullable=False)
+#    division_id = db.Column(db.Integer, db.ForeignKey('division.id'), nullable=True)
 
 
-class Division(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False)
-    school = db.Column(db.String(32), nullable=True)
-    level = db.Column(db.Integer, nullable=False)
-    students = db.relationship('User', backref='division', lazy='dynamic')
-
+#class Division(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(32), nullable=False)
+#    school = db.Column(db.String(32), nullable=True)
+#    level = db.Column(db.Integer, nullable=False)
+#    students = db.relationship('User', backref='division', lazy='dynamic')
+#
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 
